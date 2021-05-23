a6418698c739764e19a73a08cae3db0fd327c58f
Holim Lim <holim0711@outlook.com>
2021-05-23 22:43:32 +0900

    add exclude clean

--- noisy_cifar/noisy_cifar10.py
+++ noisy_cifar/noisy_cifar10.py
@@ -10,35 +10,38 @@ from .noisify import transition_matrix_cifar10, noisify
 
 class NoisyCIFAR10(pl.LightningDataModule):
 
-    def __init__(self, root, num_clean, expand_clean=False,
+    def __init__(self, root,
+                 num_clean=0, expand_clean=False, exclude_clean=False,
                  noise_type='asymmetric', noise_ratio=0.2,
                  batch_size={'clean': 0, 'noisy': 1, 'valid': None},
-                 dataloader_args={
-                     'clean': {'shuffle': True, 'num_workers': None, 'pin_memory': True},
-                     'noisy': {'shuffle': True, 'num_workers': None, 'pin_memory': True},
-                     'valid': {'shuffle': False, 'num_workers': None, 'pin_memory': True}
-                 },
                  random_seed=None):
         super().__init__()
         self.root = root
         self.num_clean = num_clean
         self.expand_clean = expand_clean
+        self.exclude_clean = exclude_clean
         self.random_state = numpy.random.RandomState(random_seed)
 
         self.T = transition_matrix_cifar10(noise_type, noise_ratio)
 
         self.batch_size = dict(batch_size)
-        train_batch_size = batch_size['clean'] + batch_size['noisy']
+
+        if self.batch_size.get('clean') is None:
+            self.batch_size['clean'] = 0
+        if self.batch_size.get('noisy') is None:
+            self.batch_size['noisy'] = 1
+
+        train_batch_size = self.batch_size['clean'] + self.batch_size['noisy']
 
         if self.batch_size.get('valid') is None:
             self.batch_size['valid'] = train_batch_size
 
-        self.dataloader_args = dict(dataloader_args)
-        if all(('num_workers' in args and args['num_workers'] is None) for args in self.dataloader_args.values()):
-            n = os.cpu_count()
-            self.dataloader_args['clean']['num_workers'] = int(n * (self.batch_size['clean'] / train_batch_size))
-            self.dataloader_args['noisy']['num_workers'] = int(n * (self.batch_size['noisy'] / train_batch_size))
-            self.dataloader_args['valid']['num_workers'] = n
+        n = os.cpu_count()
+        self.num_workers = {
+            'clean': int(n * (self.batch_size['clean'] / train_batch_size)),
+            'noisy': int(n * (self.batch_size['noisy'] / train_batch_size)),
+            'valid': n,
+        }
 
         # need to be set
         self.clean_transform = None
@@ -63,20 +66,29 @@ class NoisyCIFAR10(pl.LightningDataModule):
 
         self.noisy_cifar10 = CIFAR10(self.root, train=True, transform=self.noisy_transform)
         noisy_targets = noisify(self.noisy_cifar10.targets, self.T, self.random_state)
-        self.noisy_cifar10.targets = list(zip(noisy_targets, self.noisy_cifar10.targets))
+        self.noisy_cifar10.targets = numpy.array(list(zip(noisy_targets, self.noisy_cifar10.targets)))
+
+        if self.exclude_clean:
+            mask = numpy.ones(len(self.noisy_cifar10), dtype=bool)
+            mask[clean_indices] = False
+            self.noisy_cifar10.data = self.noisy_cifar10.data[mask]
+            self.noisy_cifar10.targets = self.noisy_cifar10.targets[mask]
 
         self.valid_cifar10 = CIFAR10(self.root, train=False, transform=self.valid_transform)
 
     def train_dataloader(self):
         loader = {}
         if self.batch_size['clean'] > 0:
-            loader['clean'] = DataLoader(self.clean_cifar10, self.batch_size['clean'], **self.dataloader_args['clean'])
+            loader['clean'] = DataLoader(self.clean_cifar10, self.batch_size['clean'],
+                                         shuffle=True, num_workers=self.num_workers['clean'], pin_memory=True)
         if self.batch_size['noisy'] > 0:
-            loader['noisy'] = DataLoader(self.noisy_cifar10, self.batch_size['noisy'], **self.dataloader_args['noisy'])
+            loader['noisy'] = DataLoader(self.noisy_cifar10, self.batch_size['noisy'],
+                                         shuffle=True, num_workers=self.num_workers['noisy'], pin_memory=True)
         return loader
 
     def val_dataloader(self):
-        return DataLoader(self.valid_cifar10, self.batch_size['valid'], **self.dataloader_args['valid'])
+        return DataLoader(self.valid_cifar10, self.batch_size['valid'],
+                          shuffle=False, num_workers=self.num_workers['valid'], pin_memory=True)
 
     def test_dataloader(self):
         return self.val_dataloader()

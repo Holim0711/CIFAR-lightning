a05aef8503827f48435783bf3123147dd59014f7
Holim Lim <holim0711@outlook.com>
2022-08-17 03:17:28 +0900

    blank

--- src/synthetic_noisy_datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-try:  # python version >= 3.7
-    from importlib.resources import read_text
-except ImportError:
-    from pkg_resources import resource_string
-
-    def read_text(package, resource):
-        return resource_string(package, resource).decode()
-
-
